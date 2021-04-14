0c0d820763458b5b38ff95a2ca6a56620049a603
X220 <apostol.sebastian.moisil@gmail.com>
2021-04-14 21:27:54 +0300

    14/04/21

---
+++ modul1.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = 'Hello World'
+print(HELLO_WORLD)
+name = "Sebastian"
+print(3 * name)
+
+name = "Sebastian"
+print(100 *( name+'\n'))
+print(print)
+
+print("  *  ")
+print(" *** ")
+print("*****"'\n''\n')
+
+print("----------")
+print("|        |")
+print("|        |")
+print("----------")
+
